34cc90b0cf4da0b3e996887abc2013ac2e60f737
Rich Apodaca <rapodaca@metamolecular.com>
2008-11-25 22:26:28 -0800

    tested tempo oxygen has no vhydrogens.

--- src/com/metamolecular/mx/test/VirtualHydrogenCounterTest.java
+++ src/com/metamolecular/mx/test/VirtualHydrogenCounterTest.java
@@ -7,6 +7,7 @@ package com.metamolecular.mx.test;
 import com.metamolecular.mx.model.Atom;
 import com.metamolecular.mx.model.DefaultMolecule;
 import com.metamolecular.mx.model.Molecule;
+import com.metamolecular.mx.model.MoleculeKit;
 import com.metamolecular.mx.model.VirtualHydrogenCounter;
 import junit.framework.TestCase;
 
@@ -129,5 +130,14 @@ public class VirtualHydrogenCounterTest extends TestCase
     molecule.connect(c, n, 1);
 
     assertEquals(1, counter.countVirtualHydrogens(n));
+  }
+  
+  public void testItShouldSayTempoOxygenAtomHasNoHydrogens()
+  {
+    String TEMPO = "[NO NAME]\n  CHEMWRIT          2D\nCreated with ChemWriter - http://metamolecular.com/chemwriter\n 11 11  0  0  0  0  0  0  0  0  0 V2000\n    7.3600   -6.6800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    8.2260   -7.1800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    9.0921   -6.6800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    9.0921   -5.6800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    8.2260   -5.1800    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0\n    7.3600   -5.800     0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    9.5921   -4.8140    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   10.0921   -5.6800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    6.8600   -4.8140    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    6.3600   -5.6800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    8.2260   -4.1800    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0  0  0  0\n  2  3  1  0  0  0  0\n  3  4  1  0  0  0  0\n  4  5  1  0  0  0  0\n  5  6  1  0  0  0  0\n  6  1  1  0  0  0  0\n  4  7  1  0  0  0  0\n  4  8  1  0  0  0  0\n  6  9  1  0  0  0  0\n  6 10  1  0  0  0  0\n  5 11  1  0  0 \nM  RAD  1  11   1\nM  END";
+    Molecule tempo = MoleculeKit.readMolfile(TEMPO);
+    
+    assertEquals("O", tempo.getAtom(10).getSymbol());
+    assertEquals(0, counter.countVirtualHydrogens(tempo.getAtom(10)));
   }
 }
