e89ac048407f4f9f1ab8a82cf1eb3db16318ef01
Richard Apodaca <rapodaca@metamolecular.com>
2008-12-11 07:58:03 -0800

    organized test sd file output

--- src/com/metamolecular/mx/test/SDFileWriterTest.java
+++ src/com/metamolecular/mx/test/SDFileWriterTest.java
@@ -7,6 +7,7 @@ package com.metamolecular.mx.test;
 import com.metamolecular.mx.io.Molecules;
 import com.metamolecular.mx.io.mdl.SDFileWriter;
 import com.metamolecular.mx.model.Molecule;
+import java.io.File;
 import junit.framework.TestCase;
 
 /**
@@ -14,9 +15,21 @@ import junit.framework.TestCase;
  */
 public class SDFileWriterTest extends TestCase
 {
-  public void testItShouldWriteASingleMoleculWithoutData() throws Exception
+  private String dir;
+  private File outputDirectory;
+  
+  public SDFileWriterTest()
   {
-    SDFileWriter writer = new SDFileWriter("ItShouldWriteASingleMoleculWithoutData.sdf");
+    dir = "test/" + getClass().getSimpleName().replace("Test", "");
+    outputDirectory = new File(dir);
+    
+    outputDirectory.mkdir();
+    cleanDir(outputDirectory);
+  }
+  
+  public void testItShouldWriteASingleMoleculeWithoutData() throws Exception
+  {
+    SDFileWriter writer = new SDFileWriter(getFilename());
     Molecule benzene = Molecules.createBenzene();
     
     writer.writeMolecule(benzene);
@@ -25,11 +38,29 @@ public class SDFileWriterTest extends TestCase
   
   public void testItShouldWriteASingleRecordWithDataAndMolfile() throws Exception
   {
-    SDFileWriter writer = new SDFileWriter("testItShouldWriteASingleRecordWithDataAndMolfile");
+    SDFileWriter writer = new SDFileWriter(getFilename());
     Molecule benzene = Molecules.createBenzene();
     
     writer.writeMolecule(benzene);
     writer.writeData("key", "value");
     writer.close();
+  }
+  
+  private String getFilename()
+  {
+    StackTraceElement[] stack = new Throwable().getStackTrace();
+    String methodName =  stack[1].getMethodName().replace("test", "");
+    
+    return dir + File.separator + methodName + ".sdf";
+  }
+  
+  private void cleanDir(File dir)
+  {
+    File[] children = dir.listFiles();
+    
+    for (File file : children)
+    {
+      file.delete();
+    }
   }
 }
