cf3e28b1208744e00bea91dc67ea92b6a68615d0
Rich Apodaca <rapodaca@metamolecular.com>
2009-06-13 12:44:39 -0700

    Atom of copied molecule returns correct index. Closes #2

--- src/com/metamolecular/mx/test/MoleculeTest.java
+++ src/com/metamolecular/mx/test/MoleculeTest.java
@@ -292,6 +292,16 @@ public class MoleculeTest extends TestCase
     
     assertEquals(0, copy.getAtom(0).getIndex());
   }
+  
+  public void testCopiedAtomHasCorrectParentMolecule()
+  {
+    Molecule molecule = new DefaultMolecule();
+    molecule.addAtom("C");
+    
+    Molecule copy = molecule.copy();
+    
+    assertEquals(copy, copy.getAtom(0).getMolecule());
+  }
 
   private class Listener implements ChangeListener
   {
